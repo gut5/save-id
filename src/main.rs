7c8b84bcb4290e9378d9cbad859da64b12f21734
guts <guts@guts.guts>
2026-06-07 19:44:47 +0100

    ui change in main.rs

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ impl App {
 }
 
 impl eframe::App for App {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    fn ui(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Game Save Scanner");
 
